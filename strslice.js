7db7a42b625ea40d7393f48b5061db1515a83915
Saikumar Reddy <135002328+SaikumarReddy69@users.noreply.github.com>
2023-05-31 15:52:10 +0530

    Final commit

---
+++ strslice.js
@@ -0,0 +1,3 @@
+let text="The is Saikumar"
+let length = text.lengt;
+console.log(index)
---
+++ strslice1.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1>
+            <script src="strslice.js"></script>
+            STRING SLICE
+        </h1>
+    </head>
+</html>
